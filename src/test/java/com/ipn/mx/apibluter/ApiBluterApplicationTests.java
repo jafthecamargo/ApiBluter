124b3d0258508d626ca25bacc21bec7163952cfe
jafthecamargo <jcamargop1600@alumno.ipn.mx>
2024-05-14 17:25:15 -0600

    Primer Comit

---
+++ src/test/java/com/ipn/mx/apibluter/ApiBluterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipn.mx.apibluter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBluterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
